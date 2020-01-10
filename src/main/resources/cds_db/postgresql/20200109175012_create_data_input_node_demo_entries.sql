f1a7ea8d7f90be4b2cafd68270cc89d58c808624
Samuel Nicolary <sam@nicolary.org>
2020-01-09 19:07:55 -0700

    update

--- src/main/resources/cds_db/postgresql/20200109175012_create_data_input_node_demo_entries.sql
+++ src/main/resources/cds_db/postgresql/20200109175012_create_data_input_node_demo_entries.sql
@@ -18,8 +18,6 @@
 -- Migration SQL that makes the change goes here.
 
 INSERT INTO data_input_node (node_id, node_path, template_name, template_class_name, attribute_name, attribute_class_name, last_mod_datetime, last_mod_id, create_datetime, create_id) 
-	VALUES ('587af9d695b4b1f47719a744fd253e55', 'demographics/birthTime', 'EvaluatedPerson', 'org.openvmr.v1_0.schema.EvaluatedPerson', 'TS', 'org.openvmr.v1_0.schema.TS', '2020-01-09 09:21:24.162', 'admin', '2020-01-09 09:21:24.162', 'admin');
-INSERT INTO data_input_node (node_id, node_path, template_name, template_class_name, attribute_name, attribute_class_name, last_mod_datetime, last_mod_id, create_datetime, create_id) 
 	VALUES ('3b9514da2bfdbf54bffb10d6cff54a95', 'demographics/gender', 'EvaluatedPerson', 'org.openvmr.v1_0.schema.EvaluatedPerson', 'CD', 'org.openvmr.v1_0.schema.CD', '2020-01-09 09:21:32.078', 'admin', '2020-01-09 09:21:32.078', 'admin');
 INSERT INTO data_input_node (node_id, node_path, template_name, template_class_name, attribute_name, attribute_class_name, last_mod_datetime, last_mod_id, create_datetime, create_id) 
 	VALUES ('46124f29305d308a6c482e948bd647dd', 'demographics/isDeceased', 'EvaluatedPerson', 'org.openvmr.v1_0.schema.EvaluatedPerson', 'BL', 'org.openvmr.v1_0.schema.BL', '2020-01-09 09:21:38.975', 'admin', '2020-01-09 09:21:38.975', 'admin');
@@ -27,4 +25,4 @@ INSERT INTO data_input_node (node_id, node_path, template_name, template_class_n
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-delete from data_input_node where node_id in ('587af9d695b4b1f47719a744fd253e55','3b9514da2bfdbf54bffb10d6cff54a95','46124f29305d308a6c482e948bd647dd');
+delete from data_input_node where node_id in ('3b9514da2bfdbf54bffb10d6cff54a95','46124f29305d308a6c482e948bd647dd');
