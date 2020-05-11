ecab43710654eaea770e8141d80b94f262cfc008
Samuel Nicolary <sam@nicolary.org>
2020-05-11 11:53:19 -0700

    some sql updates

--- resolve_pred_part_order_issue.sql
+++ resolve_pred_part_order_issue.sql
@@ -1,5 +1,19 @@
-select part_type, predicate_part_order from criteria_predicate_part where part_id in (
-    select source_part_id from condition_crit_pred_part where predicate_id =
-        (select predicate_id from condition_crit_pred_part where part_id = 'f1fc4fdfed7c0656638cf1e3dea5c0ba')
-)
+select predicate_id, predicate_part_order, count(*)
+from criteria_predicate_part
+group by predicate_id, predicate_part_order
+having count(*) > 1;
 
+select predicate_id, part_id, part_type, predicate_part_order
+from criteria_predicate_part
+where predicate_id = 'bdaa0aea849f1640f1b9add057c31d6c'
+order by predicate_part_order;
+
+select predicate_id, part_id, part_type, predicate_part_order
+from criteria_predicate_part
+where predicate_id = 'b5d6c90e172b67a44b344067f2bffe8d'
+order by predicate_part_order;
+
+select predicate_id, part_id, part_type, predicate_part_order
+from criteria_predicate_part
+where predicate_id = '684f7580d5c795b182d7c0e9d5c25595'
+order by predicate_part_order;
